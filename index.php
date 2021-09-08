7c1731446f553c4c8c578fbfc5becff75228fc58
Matteo <dottmatteocasagrande@gmail.com>
2021-09-08 13:22:53 +0200

    Array

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
 // This is a comment
-echo "Hello world!";
+echo "Hello world! </br>";
+
+$odd_numbers = [1, 3, 5, 7, 9];
+print_r($odd_numbers);
+echo count($odd_numbers);
+
+unset($odd_numbers[2]);
+print_r($odd_numbers);
+echo count($odd_numbers);
+
+
 ?>
 This is out of php tag
