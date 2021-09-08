86519efaabec3fde7e334993455e82b6d8ae19f6
Matteo <dottmatteocasagrande@gmail.com>
2021-09-08 13:33:11 +0200

    Array 2

--- index.php
+++ index.php
@@ -3,12 +3,16 @@
 echo "Hello world! </br>";
 
 $odd_numbers = [1, 3, 5, 7, 9];
-print_r($odd_numbers);
-echo count($odd_numbers);
+print_r($odd_numbers + "</br>");
+echo count($odd_numbers) + "</br>";
 
 unset($odd_numbers[2]);
-print_r($odd_numbers);
-echo count($odd_numbers);
+print_r($odd_numbers + "</br>");
+echo count($odd_numbers) + "</br>";
+
+echo reset($odd_numbers)+ "</br>";
+echo end($odd_numbers) + "</br>";
+
 
 
 ?>
