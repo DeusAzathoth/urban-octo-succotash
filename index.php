8f89c7df210eece2bc621775b6a82f8c6208b22f
Matteo <dottmatteocasagrande@gmail.com>
2021-09-09 17:29:29 +0200

    Come back

--- index.php
+++ index.php
@@ -3,15 +3,15 @@
 echo "Hello world! </br>";
 
 $odd_numbers = [1, 3, 5, 7, 9];
-print_r($odd_numbers + "</br>");
-echo count($odd_numbers) + "</br>";
+print_r($odd_numbers);
+echo count($odd_numbers);
 
 unset($odd_numbers[2]);
-print_r($odd_numbers + "</br>");
-echo count($odd_numbers) + "</br>";
+print_r($odd_numbers);
+echo count($odd_numbers);
 
-echo reset($odd_numbers)+ "</br>";
-echo end($odd_numbers) + "</br>";
+echo reset($odd_numbers);
+echo end($odd_numbers);
 
 
 
